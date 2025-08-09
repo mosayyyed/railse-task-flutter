e5a0c88b06af660544e7b709c43d900f327bfc0d
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-08-09 17:35:12 +0300

    feat: Create TaskUITaskApp widget
    This commit introduces the `TaskUITaskApp` widget, which serves as the root of the application.
    
    It sets up:
    - `BlocProvider` to provide the `TaskCubit` to the widget tree.
    - `MaterialApp` with:
        - `debugShowCheckedModeBanner` set to false.
        - A title "Task Manager".
        - `HomeScreen` as the initial screen.
        - A custom theme with blue as the primary color, and specific styling for `AppBarTheme`, `FloatingActionButtonThemeData`, and `CardThemeData`.

---
+++ lib/task_ui_task_app.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_manager_ui/core/di/service_locator.dart';
+import 'package:task_manager_ui/presentation/cubits/task_cubit.dart';
+import 'package:task_manager_ui/presentation/screens/home_screen.dart';
+
+class TaskUITaskApp extends StatelessWidget {
+  const TaskUITaskApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => serviceLocator<TaskCubit>(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Task Manager',
+        home: const HomeScreen(),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.blue,
+            titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+            foregroundColor: Colors.white,
+          ),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          cardTheme: const CardThemeData(
+            elevation: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
