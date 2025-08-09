3f4d1d4932850c5b5b29db8c7f6e56f679cfb586
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2025-08-09 18:15:11 +0300

    feat: Enhance TaskModel with Hive support and add TaskStatus/TaskPriority enums

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -1,39 +1,68 @@
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'task_model.g.dart';
 
+@HiveType(typeId: 0)
 enum TaskStatus {
+  @HiveField(0)
   @JsonValue('not_started')
   notStarted,
+  @HiveField(1)
   @JsonValue('started')
   started,
+  @HiveField(2)
   @JsonValue('completed')
   completed,
 }
 
+@HiveType(typeId: 1)
 enum TaskPriority {
+  @HiveField(0)
   @JsonValue('low')
   low,
+  @HiveField(1)
   @JsonValue('medium')
   medium,
+  @HiveField(2)
   @JsonValue('high')
   high,
 }
 
+@HiveType(typeId: 2)
 @JsonSerializable()
-class TaskModel {
+class TaskModel extends HiveObject {
+  @HiveField(0)
   final String taskId;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final String description;
+
+  @HiveField(3)
   final String assignedTo;
+
+  @HiveField(4)
   final DateTime createdDate;
+
+  @HiveField(5)
   final DateTime startDate;
+
+  @HiveField(6)
   final DateTime? dueDate;
+
+  @HiveField(7)
   final DateTime? actualEndDate;
+
+  @HiveField(8)
   final TaskStatus status;
+
+  @HiveField(9)
   final TaskPriority priority;
 
-  const TaskModel({
+  TaskModel({
     required this.taskId,
     required this.title,
     required this.description,
--- lib/data/models/task_model.g.dart
+++ lib/data/models/task_model.g.dart
@@ -3,42 +3,193 @@
 part of 'task_model.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskModelAdapter extends TypeAdapter<TaskModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  TaskModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskModel(
+      taskId: fields[0] as String,
+      title: fields[1] as String,
+      description: fields[2] as String,
+      assignedTo: fields[3] as String,
+      createdDate: fields[4] as DateTime,
+      startDate: fields[5] as DateTime,
+      dueDate: fields[6] as DateTime?,
+      actualEndDate: fields[7] as DateTime?,
+      status: fields[8] as TaskStatus,
+      priority: fields[9] as TaskPriority,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskModel obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.taskId)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.description)
+      ..writeByte(3)
+      ..write(obj.assignedTo)
+      ..writeByte(4)
+      ..write(obj.createdDate)
+      ..writeByte(5)
+      ..write(obj.startDate)
+      ..writeByte(6)
+      ..write(obj.dueDate)
+      ..writeByte(7)
+      ..write(obj.actualEndDate)
+      ..writeByte(8)
+      ..write(obj.status)
+      ..writeByte(9)
+      ..write(obj.priority);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TaskStatusAdapter extends TypeAdapter<TaskStatus> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TaskStatus read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return TaskStatus.notStarted;
+      case 1:
+        return TaskStatus.started;
+      case 2:
+        return TaskStatus.completed;
+      default:
+        return TaskStatus.notStarted;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskStatus obj) {
+    switch (obj) {
+      case TaskStatus.notStarted:
+        writer.writeByte(0);
+        break;
+      case TaskStatus.started:
+        writer.writeByte(1);
+        break;
+      case TaskStatus.completed:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskStatusAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TaskPriorityAdapter extends TypeAdapter<TaskPriority> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TaskPriority read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return TaskPriority.low;
+      case 1:
+        return TaskPriority.medium;
+      case 2:
+        return TaskPriority.high;
+      default:
+        return TaskPriority.low;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskPriority obj) {
+    switch (obj) {
+      case TaskPriority.low:
+        writer.writeByte(0);
+        break;
+      case TaskPriority.medium:
+        writer.writeByte(1);
+        break;
+      case TaskPriority.high:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskPriorityAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
 TaskModel _$TaskModelFromJson(Map<String, dynamic> json) => TaskModel(
-  taskId: json['taskId'] as String,
-  title: json['title'] as String,
-  description: json['description'] as String,
-  assignedTo: json['assignedTo'] as String,
-  createdDate: DateTime.parse(json['createdDate'] as String),
-  startDate: DateTime.parse(json['startDate'] as String),
-  dueDate: json['dueDate'] == null
-      ? null
-      : DateTime.parse(json['dueDate'] as String),
-  actualEndDate: json['actualEndDate'] == null
-      ? null
-      : DateTime.parse(json['actualEndDate'] as String),
-  status:
-      $enumDecodeNullable(_$TaskStatusEnumMap, json['status']) ??
-      TaskStatus.notStarted,
-  priority:
-      $enumDecodeNullable(_$TaskPriorityEnumMap, json['priority']) ??
-      TaskPriority.medium,
-);
+      taskId: json['taskId'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      assignedTo: json['assignedTo'] as String,
+      createdDate: DateTime.parse(json['createdDate'] as String),
+      startDate: DateTime.parse(json['startDate'] as String),
+      dueDate: json['dueDate'] == null
+          ? null
+          : DateTime.parse(json['dueDate'] as String),
+      actualEndDate: json['actualEndDate'] == null
+          ? null
+          : DateTime.parse(json['actualEndDate'] as String),
+      status: $enumDecodeNullable(_$TaskStatusEnumMap, json['status']) ??
+          TaskStatus.notStarted,
+      priority: $enumDecodeNullable(_$TaskPriorityEnumMap, json['priority']) ??
+          TaskPriority.medium,
+    );
 
 Map<String, dynamic> _$TaskModelToJson(TaskModel instance) => <String, dynamic>{
-  'taskId': instance.taskId,
-  'title': instance.title,
-  'description': instance.description,
-  'assignedTo': instance.assignedTo,
-  'createdDate': instance.createdDate.toIso8601String(),
-  'startDate': instance.startDate.toIso8601String(),
-  'dueDate': instance.dueDate?.toIso8601String(),
-  'actualEndDate': instance.actualEndDate?.toIso8601String(),
-  'status': _$TaskStatusEnumMap[instance.status]!,
-  'priority': _$TaskPriorityEnumMap[instance.priority]!,
-};
+      'taskId': instance.taskId,
+      'title': instance.title,
+      'description': instance.description,
+      'assignedTo': instance.assignedTo,
+      'createdDate': instance.createdDate.toIso8601String(),
+      'startDate': instance.startDate.toIso8601String(),
+      'dueDate': instance.dueDate?.toIso8601String(),
+      'actualEndDate': instance.actualEndDate?.toIso8601String(),
+      'status': _$TaskStatusEnumMap[instance.status]!,
+      'priority': _$TaskPriorityEnumMap[instance.priority]!,
+    };
 
 const _$TaskStatusEnumMap = {
   TaskStatus.notStarted: 'not_started',
